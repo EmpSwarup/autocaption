f4eea51156cf6065930d0fa48a59857af3b85147
Swarup Sapkota <empswarupcoc@gmail.com>
2023-12-13 16:11:14 +0545

    tailwind cofig edited, header created

--- src/app/page.js
+++ src/app/page.js
@@ -1,3 +1,21 @@
 export default function Home() {
-  return <div>test</div>;
+  return (
+    <main className="p-4 max-w-2xl mx-auto">
+      <header className="flex justify-between mb-4">
+        <a href="">AutoCaption</a>
+        <nav className="flex gap-6">
+          <a href="">Home</a>
+          <a href="">Pricing</a>
+          <a href="">Contact</a>
+        </nav>
+      </header>
+      <div>
+        <h1>Create epic captions to your videos</h1>
+        <h2>Just upload your video and we will do the rest.</h2>
+      </div>
+      <div>
+        <button>Choose file</button>
+      </div>
+    </main>
+  );
 }
