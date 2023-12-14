de82bc4b3827c86fbcc9724e78858d5b57ae98e6
Swarup Sapkota <empswarupcoc@gmail.com>
2023-12-14 12:12:58 +0545

    upload icon component created and imported

--- src/app/page.js
+++ src/app/page.js
@@ -1,4 +1,5 @@
 import SparklesIcon from "@/components/SparklesIcon";
+import UploadIcon from "@/components/UploadIcon";
 
 export default function Home() {
   return (
@@ -16,20 +17,7 @@ export default function Home() {
       </div>
       <div className="text-center">
         <button className="bg-green-600 py-2 px-6 rounded-full inline-flex gap-2 border-2 border-purple-700/50">
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            fill="none"
-            viewBox="0 0 24 24"
-            stroke-width="1.5"
-            stroke="currentColor"
-            class="w-6 h-6"
-          >
-            <path
-              stroke-linecap="round"
-              stroke-linejoin="round"
-              d="M12 16.5V9.75m0 0l3 3m-3-3l-3 3M6.75 19.5a4.5 4.5 0 01-1.41-8.775 5.25 5.25 0 0110.233-2.33 3 3 0 013.758 3.848A3.752 3.752 0 0118 19.5H6.75z"
-            />
-          </svg>
+          <UploadIcon />
           <span>Choose file</span>
         </button>
       </div>
