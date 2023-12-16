7f85e66b6df6a1ecb4d3009a3f208d190ac66e95
Swarup Sapkota <empswarupcoc@gmail.com>
2023-12-16 18:52:11 +0545

    axios added, upload api created

--- src/components/UploadForm.js
+++ src/components/UploadForm.js
@@ -1,11 +1,19 @@
 "use client";
 
 import UploadIcon from "@/components/UploadIcon";
+import axios from "axios";
 
 export default function UploadForm() {
-  function upload(ev) {
+  async function upload(ev) {
     ev.preventDefault();
-    console.log(ev);
+    const files = ev.target.files;
+    if (files.length > 0) {
+      const file = files[0];
+      const res = await axios.postForm("/api/upload", {
+        file,
+      });
+      console.log(res.data);
+    }
   }
   return (
     <label className="bg-green-600 py-2 px-6 rounded-full inline-flex gap-2 border-2 border-purple-700/50 cursor-pointer">
